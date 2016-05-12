eaacc22ee9bdeff84b2c6d9fa06e8bf86b4e6b33
Ace85340 <ryan@cycloneos.net>
2016-05-12 08:26:25 -0700

    add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -19,5 +19,37 @@ module.exports = {
 
         return $.isArray(todos) ? todos : [];
 
+    },
+    filterTodos: function (todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+
+        // filter by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+
+        // filter by searchText
+        if (searchText.length > 0) {
+            filteredTodos = filteredTodos.filter((todo) => {
+
+                var text = todo.text.toLowerCase();
+                var index = text.indexOf(searchText);
+                return index != -1;
+            });
+        }
+
+
+        // sort todos with non-completed first
+        filteredTodos.sort((a, b) => {
+            if (a.completed === false && b.completed === true) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        return filteredTodos;
     }
 };
